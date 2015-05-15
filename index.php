e8c0d216b824c092c76547f246e39422bd32753b
a.karpov <karpovdev@gmail.com>
2015-05-15 09:54:28 +0300

    some edit

--- index.php
+++ index.php
@@ -6,4 +6,15 @@ require_once 'BL/Autoload.php';
 
 use SD\Type\IType;
 
-//phpinfo();die;
+$statReq = \SD\Model\Entity\Composite::createFromArray([
+    'provider' => new \SD\Type\Str('stat'),
+    'operation' => new \SD\Type\Str('sales')
+]);
+
+use SP\Mixin\EngineTool;
+class Example extends \SP\Gear\Web{
+    use SP\Mixin\EngineTool;
+}
+
+$e = new Example();
+$e->collectStat($statReq);
