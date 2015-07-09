14fe7e877704a19525c1e334e1f7de170e29b6f0
a.karpov <karpovdev@gmail.com>
2015-07-09 18:53:39 +0300

    some edit

--- index.php
+++ index.php
@@ -6,4 +6,26 @@ require_once 'BL/Autoload.php';
 
 use SD\Type\IType;
 
-//phpinfo();die;
+
+$request = \SD\Model\Entity\Composite::createFromArray(array(
+    'provider' => new \SD\Type\Str('burs'),
+    //'operation' => new \SD\Type\Str('getBusStops'),
+    'operation' => new \SD\Type\Str('getAllBusStops'),
+    //'operation' => new \SD\Type\Str('getBusTrips'),
+    /*'params' => \SD\Model\Entity\Spec::createFromArray([
+        'BusStopName' => 'Москв',
+    ])*/
+));
+
+/*$request = \SD\Model\Entity\Composite::createFromArray(array(
+    'provider' => new \SD\Type\Str('burs'),
+    'operation' => new \SD\Type\Str('getBusTrips'),
+    'params' => \SD\Model\Entity\Spec::createFromArray([
+        'DepartureBusStopID' => '23647',
+        'ArrivalBusStopID' => '23650',
+        'DepartureDate' => '2015-07-21',
+    ])
+));*/
+
+$resp = \SD\Service::getInstance($request)->run();
+var_dump($resp->response);
