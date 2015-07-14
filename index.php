d6e912ed8f8cafde54245a695b7bdd293528c56a
a.karpov <karpovdev@gmail.com>
2015-07-14 12:33:10 +0300

    some edit

--- index.php
+++ index.php
@@ -29,35 +29,36 @@ $request = \SD\Model\Entity\Composite::createFromArray(array(
 
 /*$request = \SD\Model\Entity\Composite::createFromArray(array(
     'provider' => new \SD\Type\Str('burs'),
-    'operation' => new \SD\Type\Str('bookOrder'),
-    'params' => \SD\Model\Entity\Spec::createFromArray([
-        'RunID' => 'MjM0MDJfTVRBd01EUjhNVEF3TURGOE1qZ3VNRGN1TWpBeE5TQXdPakF3T2pBd2ZIeEhSRk14ZkRFd05URT0=',
-        'DepartureBusStopID' => '23647',
-        'ArrivalBusStopID' => '23650',
-        'DepartureDate' => '2015-07-28',
-        'Passengers' => \SD\Model\Entity::createFromArray([
-            'Name' => 'Алексей',
-            'MiddleName' => 'Николаевич',
-            'Surname' => 'Карпов',
-            'DocumentNumber' => '54364536452',
-            'Birthdate' => '1987-07-08',
-            'BirthPlace' => 'Севастополь',
-            //'CountryDigitalCode' => '',
-            'Gender' => 'm', // f | m
-            'TariffCode' => '',
-            'SeatNumber' => '',
-        ])
-    ])
-));*/
-
-$request = \SD\Model\Entity\Composite::createFromArray(array(
-    'provider' => new \SD\Type\Str('burs'),
     'operation' => new \SD\Type\Str('getBookParams'),
     'params' => \SD\Model\Entity\Spec::createFromArray([
         'RunID' => 'MjM0MDJfTVRBd01EUjhNVEF3TURGOE1qZ3VNRGN1TWpBeE5TQXdPakF3T2pBd2ZIeEhSRk14ZkRFd05URT0=',
         'DepartureBusStopID' => '23647',
         'ArrivalBusStopID' => '23650',
         'DepartureDate' => '2015-07-28',
+    ])
+));*/
+
+$request = \SD\Model\Entity\Composite::createFromArray(array(
+    'provider' => new \SD\Type\Str('burs'),
+    'operation' => new \SD\Type\Str('bookOrder'),
+    'params' => \SD\Model\Entity\Spec::createFromArray([
+
+        'RunID' => 'MjM0MDJfTVRBd01EUjhNVEF3TURGOE1qZ3VNRGN1TWpBeE5TQXdPakF3T2pBd2ZIeEhSRk14ZkRFd05URT0=',
+        'DepartureBusStopID' => '23647',
+        'ArrivalBusStopID' => '23650',
+        'DepartureDate' => '2015-07-28',
+        'Passenger' => \SD\Model\Entity\Spec::createFromArray([
+            'Name' => 'Петр',
+            'MiddleName' => 'Сергеевич',
+            'Surname' => 'Дуров',
+            'DocumentNumber' => '54364536452',
+            'Birthdate' => '07-08-1987',
+            'BirthPlace' => 'Севастополь',
+            'CountryDigitalCode' => '643',
+            'Gender' => 'm', // f | m
+            'TariffCode' => '55282',
+            'SeatNumber' => '1',
+        ]),
     ])
 ));
 
