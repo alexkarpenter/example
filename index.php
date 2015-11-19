a3d921ff4c4b83d0b44ec7483f558364c532331f
a.karpov <karpovdev@gmail.com>
2015-11-19 19:53:27 +0300

    some edit

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ ini_set("display_errors", 1);
 
 require_once 'BL/Autoload.php';
 
-//$dsn = new \BL\Dsn\Pgsql();
-
-//$adapter = new BL\Db\Adapter\Sql\Adapter;
-
 $conConnect = [
     'driver' => 'pgsql',
     'host' => '89.175.169.162',
@@ -20,21 +16,74 @@ $conConnect = [
 
 $dsn = \BL\Dsn::create($conConnect['driver'], $conConnect);
 $adapter = \BL\Db\Adapter::create($dsn);
-$qb = \BL\Db\QueryBuilder\Sql::create($adapter);
 $provider = new \BL\Db\QueryBuilder\Sql($adapter, null, 'tol');
-$qb->setTable('vw_user');
+
+
+$twoWeeksAgo = date('Y-m-d', strtotime("-2 week"));
+
 $res = $provider->select()
     ->from('vw_user')
-    ->where([])
-    ->limit(10)
+    ->where(
+        [
+            new \BL\Db\QueryBuilder\Sql\Predicate\Operator(
+                'created',
+                \BL\Db\QueryBuilder\Sql\Predicate\Operator::GREATER_THAN,
+                $twoWeeksAgo
+            ),
+            new \BL\Db\QueryBuilder\Sql\Predicate\Operator(
+                'agent_id',
+                \BL\Db\QueryBuilder\Sql\Predicate\Operator::EQUAL_TO,
+                3)
+        ]
+    )
+    //->limit(10) // todo убрать
     ->execute()
     ->fetchAll();
 
+if ($res && count($res) > 0) {
+    echo 'Извлечено записей: '.count($res);
+} else {
+    die('Нет данных от User');
+}
 
+$strBuf = "Имя;Фамилия;Дата рождения;Город;Пол;Номер телефона;E-mail;EAN\n";
+foreach ($res as $row) {
+    $strBuf .= $row['first_name'].';';
+    $strBuf .= $row['last_name'].';';
+    $strBuf .= $row['birth'].';';
+    $strBuf .= ';';             // City
+    $strBuf .= $row['sex'].';';
+    $strBuf .= $row['phone'].';';
+    $strBuf .= $row['email'].';';
+    $strBuf .= $row['kuza'];
+    //$strBuf .= $row['created'];
+    $strBuf .= "\n";
+}
 
-$res = \SD\Service::getInstance($req)->run();
+$file = 'testfile.csv';
+$fp = fopen($file, "w");
+fwrite($fp, $strBuf);
 
-print_r($res->response);
+// установка соединения
+$conn_id = ftp_connect('ftp.euroset.ru', 21);
+
+// вход на ftp
+$login_result = ftp_login($conn_id, 'bonline', 'aES5VRZoXfco');
+
+if ((!$conn_id) || (!$login_result)) {
+    die('Не удается соединиться по ftp');
+}
+
+// попытка закачивания файла
+if (ftp_fput($conn_id, '.', $fp, FTP_ASCII)) {
+    echo "Файл $file успешно загружен\n";
+} else {
+    echo "При закачке $file произошла проблема\n";
+}
+
+// закрываем соединение и дескриптор файла
+ftp_close($conn_id);
+fclose($fp);
 
 /*select "first_name", "last_name", "birth", 'city', "sex", "phone", "email", "kuza", "created"
 from tol."user"
