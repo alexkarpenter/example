fb74fd0632e3469c01563127d65ad86e5cb17097
a.karpov <karpovdev@gmail.com>
2015-11-19 16:16:14 +0300

    some edit

--- index.php
+++ index.php
@@ -4,6 +4,39 @@ ini_set("display_errors", 1);
 
 require_once 'BL/Autoload.php';
 
-use SD\Type\IType;
+//$dsn = new \BL\Dsn\Pgsql();
 
-//phpinfo();die;
+//$adapter = new BL\Db\Adapter\Sql\Adapter;
+
+$conConnect = [
+    'driver' => 'pgsql',
+    'host' => '89.175.169.162',
+    'port' => '5432',
+    'username' => 'bol',
+    'password' => 'gjasghuAFwgtsd41',
+    'dbname' => 'bol',
+    'schema' => 'bol'
+];
+
+$dsn = \BL\Dsn::create($conConnect['driver'], $conConnect);
+$adapter = \BL\Db\Adapter::create($dsn);
+$qb = \BL\Db\QueryBuilder\Sql::create($adapter);
+$provider = new \BL\Db\QueryBuilder\Sql($adapter, null, 'tol');
+$qb->setTable('vw_user');
+$res = $provider->select()
+    ->from('vw_user')
+    ->where([])
+    ->limit(10)
+    ->execute()
+    ->fetchAll();
+
+
+
+$res = \SD\Service::getInstance($req)->run();
+
+print_r($res->response);
+
+/*select "first_name", "last_name", "birth", 'city', "sex", "phone", "email", "kuza", "created"
+from tol."user"
+where "agent_id" = 4 and
+"created" > '2015-10-01';*/
